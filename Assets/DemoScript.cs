2364185f79d1733630c92e81010f8e81350332f6
arshad <hussain.arshad4u@gmail.com>
2015-12-16 12:44:28 +0530

    First commit

---
+++ Assets/DemoScript.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using plakcDIGALite;
+
+public class DemoScript : MonoBehaviour {
+
+	public int NextScene;
+	// Use this for initialization
+	void Start () 
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	void OnGUI()
+	{
+		if(GUI.Button(new Rect(Screen.width - 100, 0, 100, 100),"NextScene : "+NextScene))
+		{
+			GameObject go = GameObject.FindGameObjectWithTag("PROPHET");
+			go.GetComponent<PROPHET>().ProphetRequest(true,NextScene);
+		}
+		if(GUI.Button(new Rect(0, 0, 100, 100),"EXIT"))
+		{
+			GameObject go = GameObject.FindGameObjectWithTag("PROPHET");
+			go.GetComponent<PROPHET>().ProphetRequest(true,-1);
+		}
+	}
+}
